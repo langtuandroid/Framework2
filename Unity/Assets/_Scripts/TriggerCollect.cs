f4045e9df906833fe6755c7674653734785b1481
王二 <18530117331@163.com>
2023-04-25 23:46:53 +0800

    加入生成触发器

---
+++ Unity/Assets/_Scripts/TriggerCollect.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerCollect : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print($"{other.name} {Time.frameCount}");
+    }
+}
