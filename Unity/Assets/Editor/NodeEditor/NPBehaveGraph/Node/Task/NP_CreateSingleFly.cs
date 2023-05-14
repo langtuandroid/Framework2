3b4d784ffd988a1161ec50eef43d1e12f4daf651
王二 <18530117331@163.com>
2023-05-14 23:38:29 +0800

    创建飞行物

---
+++ Unity/Assets/Editor/NodeEditor/NPBehaveGraph/Node/Task/NP_CreateSingleFly.cs
@@ -0,0 +1,16 @@
+﻿using GraphProcessor;
+
+[NodeMenuItem("NPBehave行为树/Task/发射单一飞行物", typeof (NPBehaveGraph))]
+[NodeMenuItem("NPBehave行为树/Task/发射单一飞行物", typeof (SkillGraph))]
+public class NP_CreateSingleFly : NP_TaskNodeBase
+{
+     public override string name => "发射单一飞行物";
+ 
+     public NP_ActionNodeData NP_ActionNodeData =
+         new NP_ActionNodeData() { NpClassForStoreAction = new NP_AddBuffAction() };
+ 
+     public override NP_NodeDataBase NP_GetNodeData()
+     {
+         return NP_ActionNodeData;
+     }       
+}
---
+++ Unity/Assets/_Scripts/Battle/NPBehave/NodeData/Action/NP_CreateSingleFlyAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Sirenix.OdinInspector;
+
+[Title("创建单一飞行物", TitleAlignment = TitleAlignments.Centered)]
+public class NP_CreateSingleFlyAction : NP_ClassForStoreAction
+{
+
+    [LabelText("飞行物prefab")]
+    public string FlyPath;
+
+    [LabelText("飞行物出生点")]
+    public string HangPoint;
+
+    [LabelText("飞行速度")]
+    public float Speed;
+    
+    public bool 
+    
+    public override Action GetActionToBeDone()
+    {
+        this.Action = this.Create;
+        return this.Action;
+    }
+
+    private void Create()
+    {
+    }
+}
