cf6e8882c5067d4c7ce3a627baee8bee488866d7
wanger <18530117331@163.com>
2023-09-04 21:17:21 +0800

    增加跟随的目标销毁

---
+++ Unity/Assets/Editor/NodeEditor/NPBehaveGraph/Node/Task/NP_WaitUnitDisposedActionNode.cs
@@ -0,0 +1,22 @@
+﻿using GraphProcessor;
+
+[NodeMenuItem("NPBehave行为树/Task/等待Unit销毁", typeof(SkillGraph))]
+[NodeMenuItem("NPBehave行为树/Task/等待Unit销毁", typeof(NPBehaveGraph))]
+public class NP_WaitUnitDisposedActionNode : NP_TaskNodeBase
+{
+    public override string name => "等待Unit销毁";
+
+    public NP_ActionNodeData NP_ActionNodeData = new() { NpClassForStoreAction = new NP_WaitUnitDisposedAction() };
+
+    public override NP_NodeDataBase NP_GetNodeData()
+    {
+        return NP_ActionNodeData;
+    }
+
+    public override string CreateNodeName => nameof(NP_WaitUnitDisposedAction);
+
+    public override void Debug_SetNodeData(object data)
+    {
+        NP_ActionNodeData = (NP_ActionNodeData)data;
+    }
+}
---
+++ Unity/Assets/_Scripts/Battle/NPBehave/GameAction/NP_WaitUnitDisposedAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Framework;
+using Action = NPBehave.Action;
+
+public class NP_WaitUnitDisposedAction : NP_ClassForStoreAction
+{
+    public NP_BlackBoardRelationData<long> UnitId = new NP_BlackBoardRelationData<long>();
+    private Unit unit;
+
+    public override Func<bool, Action.Result> GetFunc2ToBeDone()
+    {
+        return UntilFunc;
+    }
+
+    private Action.Result UntilFunc(bool isCancel)
+    {
+        if (isCancel)
+        {
+            return Action.Result.SUCCESS;
+        }
+
+        if (unit == null)
+        {
+            unit = BelongToUnit.Domain.GetComponent<UnitComponent>()
+                .Get(UnitId.GetBlackBoardValue(BelongtoRuntimeTree.GetBlackboard()));
+        }
+
+        if (unit.IsDisposed)
+        {
+            return Action.Result.SUCCESS;
+        }
+
+        return Action.Result.PROGRESS;
+    }
+}
